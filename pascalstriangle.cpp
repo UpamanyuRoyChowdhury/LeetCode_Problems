9912f0d5e7d3857cf345366fd750bfe4d7eff795
Upamanyu Roy Chowdhury <102761622+UpamanyuRoyChowdhury@users.noreply.github.com>
2023-06-12 12:51:19 +0000

    New Commit

---
+++ pascalstriangle.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) 
+    {
+        vector<vector<int>> res1;
+        for (int i = 0; i < numRows; i++)
+        {
+            vector<int> res2;
+            for(int j = 0; j <= i; j++)
+            {
+                if ((j == 0) || (j == i))
+                {
+                    res2.push_back(1);
+                }
+                else 
+                {
+                    res2.push_back(res1[i - 1][j - 1] + res1[i - 1][j]);
+                }
+            }
+            res1.push_back(res2);
+        }
+        return res1;
+    } 
+};
