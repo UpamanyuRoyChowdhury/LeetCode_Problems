0fa3110c9287e525652cc392e21a8599971c2b0e
Upamanyu Roy Chowdhury <102761622+UpamanyuRoyChowdhury@users.noreply.github.com>
2023-06-24 04:46:15 +0530

    Create finalValueAfterOperations.cpp

---
+++ finalValueAfterOperations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) 
+    {
+        int res = 0;
+        for (int i = 0; i < operations.size(); i++)
+        {
+            if (operations[i] == "--X" || operations[i] == "X--")
+            {
+                res = res - 1;
+            }
+            else if (operations[i] == "++X" || (operations[i] == "X++"))
+            {
+                res = res + 1;
+            }
+        }
+        return res;
+    }
+};
