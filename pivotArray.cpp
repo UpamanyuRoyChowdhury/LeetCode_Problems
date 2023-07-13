c76bb8f7ebb09b82660fb47f6df71c9006db97e3
Upamanyu Roy Chowdhury <102761622+UpamanyuRoyChowdhury@users.noreply.github.com>
2023-07-13 21:50:59 +0530

    Create pivotArray.cpp

---
+++ pivotArray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) 
+    {
+        vector <int> left;
+        vector <int> right;
+        vector <int> middle;
+        for (int i = 0; i < nums.size();i++)
+        {
+            if (nums[i] < pivot)
+            {
+                left.push_back(nums[i]);
+            }
+            else if (nums[i] == pivot)
+            {
+                middle.push_back(nums[i]);
+            }
+            else
+            {
+                right.push_back(nums[i]);
+            }
+        }
+        left.insert(left.end(), middle.begin(), middle.end());
+        left.insert(left.end(), right.begin(), right.end());
+        return left;
+    }
+};
