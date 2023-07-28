dc74577f8b585eb1ad24e5a82bc942880424aa12
Upamanyu Roy Chowdhury <102761622+UpamanyuRoyChowdhury@users.noreply.github.com>
2023-07-28 23:53:55 +0530

    Create onesMinusZeros.cpp

---
+++ onesMinusZeros.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) 
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> onesRow(m, 0);
+        vector<int> onesCol(n, 0);
+        vector<int> zerosRow(m, 0);
+        vector<int> zerosCol(n, 0);
+
+        // Calculate the number of ones and zeros in each row and column
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                if (grid[i][j] == 1) 
+                {
+                    onesRow[i]++;
+                    onesCol[j]++;
+                } 
+                else 
+                {
+                    zerosRow[i]++;
+                    zerosCol[j]++;
+                }
+            }
+        }
+
+        // Generate the difference matrix 'diff'
+        vector<vector<int>> diff(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                diff[i][j] = onesRow[i] + onesCol[j] - zerosRow[i] - zerosCol[j];
+            }
+        }
+
+        return diff;
+    }
+};
